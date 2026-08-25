f0489457fed17221bb3c02e6e5a34faea68f1472
blindspotinsali-source <blindspotinsali@gmail.com>
2026-08-25 14:19:06 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Bem-vindo ao GeoStar!')
+
+#Importar biblioteca Matplotlib
+import matplotlib.pyplot as plt
+#Ponto A
+x1 = -4
+y1 = 3
+#Ponto B
+x2 = 2
+y2 = 5
+#Ponto C
+x3 = 5
+y3 = -2
+#Apelido plt
+plt.scatter(x1,y1, marker='*')
+plt.scatter(x2,y2,marker='*')
+plt.scatter(x3,y3)
+plt.show()
