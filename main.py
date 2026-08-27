8572b087718be2c89c71ae3c7136b6b95a9443e3
blindspotinsali-source <blindspotinsali@gmail.com>
2026-08-27 14:25:57 -0300

    feat main

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-print('Bem-vindo ao GeoStar!')
+import pyxel
+class Estrela:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-#Importar biblioteca Matplotlib
-import matplotlib.pyplot as plt
-#Ponto A
-x1 = -4
-y1 = 3
-#Ponto B
-x2 = 2
-y2 = 5
-#Ponto C
-x3 = 5
-y3 = -2
-#Apelido plt
-plt.scatter(x1,y1, marker='*', s=100)
-plt.scatter(x2,y2,marker='*', s=100)
-plt.scatter(x3,y3, marker='*', s=100)
-plt.show()
+    def desenhar(self):
+        tela_x, tela_y = self.converter_coordenada()
+        pyxel.text(tela_x, tela_y, '*', 7)
+
+    def converter_coordenada(self):
+        tela_x = 200 + self.x * 20#Corrdenada matemática / 200 Centro da tela
+        tela_y = 150 - self.y * 20#20 Cada um unidade matemática vale 20 pixels
+        return tela_x, tela_y
+    
+class Jogo:
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.text(100, 100,'GeoStar', 7)
+        pyxel.text(100, 110, 'Desafio das Retas', 7)
+        for x in range(0, 401, 20):#Criar grade
+            pyxel.line(x, 0, x, 300, 1)
+        for y in range(0 , 301, 20):#Criar grade
+            pyxel.line(0, y, 400, y, 1)
+        pyxel.line(0, 150, 400, 150, 7)#Linha orizontal
+        pyxel.line(200, 0, 200, 300, 7)#Linha vertical
+        estrela1.desenhar()
+        estrela2.desenhar()
+        estrela3.desenhar()
+        estrela4.desenhar()
+estrela1 = Estrela(1, -1)
+estrela2 = Estrela(3, 2)
+estrela3 = Estrela(-4, 3)
+estrela4 = Estrela(5, -2)
+
+class Reta:
+    def __init__(self,a, b):
+        self.a = a
+        self.b = b
+        
+    def calcular_y(self,x):
+        return self.a * x + self.b#Uma função matemática transformado em código
+reta1 = Reta(2,1)
+print((reta1.calcular_y(3)))
+jogo = Jogo()
+pyxel.init(400, 300, title='GeoStar\033') #Cria a configuração de janela.
+pyxel.run(jogo.update, jogo.draw)#Responsável pela lógica do jogo e responsável por desenhar na tela.
+
 
