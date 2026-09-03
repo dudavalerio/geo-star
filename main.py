140536ba4863fe732bded099326ad939582d8741
blindspotinsali-source <blindspotinsali@gmail.com>
2026-09-03 00:46:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class Estrela:
             tela_x, tela_y = self.converter_coordenada()
             pyxel.text(tela_x, tela_y, '*', 7)
     def converter_coordenada(self):
-        tela_x = 200 + self.x * 20
-        tela_y = 150 - self.y * 20
+        tela_x = 250 + self.x * 20
+        tela_y = 170 - self.y * 20
         return tela_x, tela_y
 
 class Reta:
@@ -22,24 +22,42 @@ class Reta:
     def passa_por(self, estrela):
         y_calculado = self.calcular_y(estrela.x)
         return y_calculado == estrela.y
+    #Vericar todas as estrelas quando encontrar ele coleta
+    def coletar_estrelas(self, estrelas):
+        for estrela in estrelas:
+            if self.passa_por(estrela):
+                estrela.coletada = True
     def desenhar(self):
         anterior_x = None
         anterior_y = None
         for x in range(-10, 11):
             y = self.calcular_y(x)
-            tela_x = 200 + x * 20
-            tela_y = 150 - y * 20
+            tela_x = 250 + x * 20
+            tela_y = 170 - y * 20
             if anterior_x is not None:
                 pyxel.line(anterior_x, anterior_y, tela_x, tela_y, 8)
             anterior_x = tela_x
             anterior_y = tela_y
-estrelas = [Estrela(1,-1), Estrela(3,2), Estrela(-4,3), Estrela(5,-2)]
+estrelas = [Estrela(1,-1), Estrela(3,2), Estrela(-4,3), Estrela(5,-2), Estrela(2,5)]
 reta1 = Reta(2, 1)
+reta1.coletar_estrelas(estrelas)
 
 class Jogo:
+    #Contador de estrelas, ele tem que mostrar números de estrelas coletados
+    def contar_estrelas(self, estrelas):
+        total_de_estrelas = 0
+        for estrela in estrelas:
+            if estrela.coletada:
+                 total_de_estrelas += 1
+        return total_de_estrelas
     def update(self):
         pass
     def draw(self):
+        #Painel Superior
+        pyxel.rect(0, 0, 100, 300, 1)
+        #pyxel.text(10, 10, 'GeoStar', 7)
+        total_de_estrelas = self.contar_estrelas(estrelas)
+        pyxel.text(10,20,f'Estrelas: {total_de_estrelas}/ {len(estrelas)}',7)
         # Grade
         for x in range(0, 401, 20):
             pyxel.line(x, 0, x, 300, 1)
@@ -47,8 +65,8 @@ class Jogo:
         for y in range(0, 301, 20):
             pyxel.line(0, y, 400, y, 1)
         # Eixos
-        pyxel.line(0, 150, 400, 150, 7)
-        pyxel.line(200, 0, 200, 300, 7)
+        pyxel.line(100, 170, 400, 170, 7)
+        pyxel.line(250, 40, 250, 300, 7)
         # Reta
         reta1.desenhar()
         # Estrelas
