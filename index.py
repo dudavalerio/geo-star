ae42233cc06fd63ac66300d03cb07bd872c231e5
blindspotinsali-source <blindspotinsali@gmail.com>
2026-09-08 15:49:39 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,10 +6,12 @@ class Estrela:
         self.x = x#Atributo
         self.y = y
         self.coletada = False
+
     def desenhar(self):
         if not self.coletada:
             tela_x, tela_y = self.converter_coordenada()#Chamou a função
             pyxel.text(tela_x, tela_y, '*', 7)
+
     def converter_coordenada(self):
         tela_x = 250 + self.x * 20
         tela_y = 170 - self.y * 20
@@ -52,12 +54,7 @@ class Jogo:
         self.reta1 = Reta(1, 3)
         
         # Lista de estrelas guardada dentro do jogo
-        self.estrelas = [
-            Estrela(1, -1),
-            Estrela(3, 2),
-            Estrela(-4, 3),
-            Estrela(5, -2)
-        ]
+        self.estrelas = [Estrela(1, -1),Estrela(3, 2),Estrela(-4, 3),Estrela(5, -2)]
         
         pyxel.run(self.update, self.draw)
     
@@ -151,16 +148,6 @@ class Jogo:
         
         pyxel.mouse(True) #habilita o mouse em cima da janela
         
-        # Desenha a Grade Azul (1)
-        #for x in range(0, 401, 20):
-        #    pyxel.line(x, 0, x, 300, 1)
-        #for y in range(0, 301, 20):
-        #    pyxel.line(0, y, 400, y, 1)
-            
-        # Desenha os Eixos Principais Brancos (7)
-        #pyxel.line(0, 150, 400, 150, 7) # Eixo X
-        #pyxel.line(200, 0, 200, 300, 7) # Eixo Y
-        
          # Grade Vertical
         for x in range(-7, 8):
             tela_x = 250 + x * 20
@@ -185,10 +172,6 @@ class Jogo:
             if y != 0:
                 tela_y = 170 - y * 20#Calculamos onde ele deve aparecer na tela
                 pyxel.text(254, tela_y, str(y), 9)
-
-        
-       
-
 # Inicializa o jogo diretamente
 Jogo()
 
